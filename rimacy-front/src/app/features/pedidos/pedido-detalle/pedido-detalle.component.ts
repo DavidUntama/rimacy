04057675361a7f42483758e37f4e99271ff57225
DavidUntama <david.uhx64@gmail.com>
2024-12-06 18:00:30 -0500

    implementacion service pedido, formulario ingreso pedido master avanzado2

--- rimacy-front/src/app/features/pedidos/pedido-detalle/pedido-detalle.component.ts
+++ rimacy-front/src/app/features/pedidos/pedido-detalle/pedido-detalle.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PenCurrencyPipe } from '../../../pipes/penCurrencyPipe';
+import { AutocompleteComponent } from '../../../components/autocomplete/autocomplete.component';
+import { itemsDTO } from '../../../models/ItemsDTO';
+import { ProductosService } from '../../../services/productos.service';
 
 @Component({
   selector: 'pedido-detalle',
   standalone: true,
-  imports: [PenCurrencyPipe],
+  imports: [PenCurrencyPipe, AutocompleteComponent],
   templateUrl: './pedido-detalle.component.html',
   styleUrl: './pedido-detalle.component.css'
 })
 export class PedidoDetalleComponent {
+  prodsSrv = inject(ProductosService);
 
+  items: itemsDTO[] = [];
+  ngOnInit(): void {
+    this.prodsSrv.getProductos().subscribe({
+      next: (data) => {
+        data.map((prod) => {
+          this.items.push({ id: prod.id, nombre: prod.denominacion });
+        });
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    })
+  }
 }
